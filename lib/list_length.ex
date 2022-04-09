7ab137c1c2fffa79354dea44652d08f5d86c6812
Andre Chamis <andrechamis21@gmail.com>
2022-04-08 22:05:56 -0300

    Project Finished

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: calc_lenght(list, 0)
+
+  defp calc_lenght([], acc) do
+    acc
+  end
+
+  defp calc_lenght([_ | tail], acc) do
+    calc_lenght(tail, acc + 1)
   end
 end
